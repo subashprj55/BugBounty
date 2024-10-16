c4b036593428fc75d6521aa4e1a80b481d6e5543
subashprj55 <99418785+subashprj55@users.noreply.github.com>
2024-10-16 18:30:43 +0545

    Responsive NavBar (#4)
    Co-authored-by: subashprj55 <Sonikgau@1gau>

--- src/components/BugNavbar/index.js
+++ src/components/BugNavbar/index.js
@@ -1,15 +1,65 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  StyledAppBar,
+  StyledImg,
+  StyledLinksStack,
+  StyledLogoBox,
+  StyledNavLink,
+  StyledNavLinksBox,
+  StyledNavLinksStack,
+  StyledNavStack,
+  StyledStack,
+} from "./style";
+import logo from "Images/logo.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 const BugNavbar = () => {
-  return (
-    <div>
-      <h3> this is nav bar</h3>
-      <Link to={"/"}>home</Link>
-      <br></br>
+  const [isNavOpen, setIsNavOpen] = useState(true);
 
-      <Link to={"/test"}>test</Link>
-    </div>
+  return (
+    <>
+      <StyledAppBar position="sticky">
+        <StyledStack>
+          <StyledLogoBox>
+            <StyledImg src={logo} alt="logo" />
+          </StyledLogoBox>
+          {/*  this links display on tab and laptop version */}
+          <StyledLinksStack>
+            <StyledNavLink to={"/"}>Dashboard</StyledNavLink>
+            <StyledNavLink to={"/activeBounties"}>
+              Active Bounties
+            </StyledNavLink>
+            <StyledNavLink to={"/submitBug"}>Submit Bug</StyledNavLink>
+            <StyledNavLink to={"/rewards"}>Rewards</StyledNavLink>
+            <StyledNavLink to={"/leaderBoard"}>LeaderBoard</StyledNavLink>
+          </StyledLinksStack>
+          {/* Side bar toggle button */}
+          <StyledNavStack>
+            {isNavOpen ? (
+              <CloseIcon onClick={() => setIsNavOpen(false)} />
+            ) : (
+              <MenuIcon onClick={() => setIsNavOpen(true)} />
+            )}
+          </StyledNavStack>
+        </StyledStack>
+      </StyledAppBar>
+      {/* this is links for mobile dives */}
+
+      <StyledNavLinksStack>
+        <StyledNavLinksBox
+          className={`${isNavOpen ? "openNavbar" : "closeNavbar"}`}
+        >
+          <StyledNavLink className="active" to={"/"}>
+            Dashboard
+          </StyledNavLink>
+          <StyledNavLink to={"/activeBounties"}>Active Bounties</StyledNavLink>
+          <StyledNavLink to={"/submitBug"}>Submit Bug</StyledNavLink>
+          <StyledNavLink to={"/rewards"}>Rewards</StyledNavLink>
+          <StyledNavLink to={"/leaderBoard"}>LeaderBoard</StyledNavLink>
+        </StyledNavLinksBox>
+      </StyledNavLinksStack>
+    </>
   );
 };
 
