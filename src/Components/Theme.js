2e81f1d1c15001dcd05d858fa1f4343cdf4523ea
subashprj55 <99418785+subashprj55@users.noreply.github.com>
2024-10-18 17:14:59 +0545

    withdraw token section (#10)
    Co-authored-by: subashprj55 <Sonikgau@1gau>

--- src/Components/Theme.js
+++ src/Components/Theme.js
@@ -3,6 +3,18 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
   palette: {},
   typography: {
+    h1: {
+      fontFamily: "Epilogue",
+      fontSize: "25px",
+      fontWeight: 700,
+      lineHeight: "38px",
+    },
+    h2: {
+      fontFamily: "Epilogue",
+      fontSize: "20px",
+      fontWeight: 700,
+      lineHeight: "30px",
+    },
     h3: {
       fontSize: "14px",
       fontWeight: "600",
@@ -15,11 +27,36 @@ export const theme = createTheme({
         letterSpacing: "0.15px",
       },
     },
+    h6: {
+      fontFamily: "Inter",
+      fontSize: "15px",
+      fontWeight: 400,
+      lineHeight: "20px",
+    },
     footer: {
       fontSize: "16px",
       fontWeight: "400",
       lineHeight: "24px",
       letterSpacing: "0.17px",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        contained: {
+          width: "100%",
+          backgroundColor: "#39D2C0",
+          color: "#101213 ",
+          fontSize: "14px",
+          padding: "10px 20px",
+          borderRadius: "15px",
+          textTransform: "capitalize",
+          "&:hover": {
+            backgroundColor: "#3dc4b4",
+            color: "white",
+          },
+        },
+      },
     },
   },
 });
---
+++ src/Pages/BugAboutUs/style.js
@@ -0,0 +1,63 @@
+const { Typography, Stack, Box, Button } = require("@mui/material");
+const { styled } = require("styled-components");
+
+export const StyledAboutUsPage = styled.div`
+  background-color: #f1f1f1;
+  width: 100vw;
+  height: 100%;
+  @media (max-width: 768px) {
+    margin-left: 3%;
+  }
+`;
+
+export const StyledTypography = styled(Typography)`
+  &.bold {
+    font-weight: 700;
+  }
+  &.space {
+    padding: 7px 0;
+  }
+  &.color {
+    color: #898494;
+  }
+`;
+
+export const StyledWithdrawSection = styled.div`
+  margin-left: 100px;
+  margin-top: 50px;
+`;
+
+export const StyledWithdrawBox = styled(Box)`
+  margin: 15px 0;
+  width: 630px;
+  background-color: white;
+  padding: 15px;
+  border-radius: 12px;
+  border: 1px solid #e4e4e7;
+  @media (max-width: 768px) {
+    width: 530px;
+  }
+`;
+
+export const StyledBox = styled(Box)`
+  margin-top: 30px;
+  padding-bottom: 15px;
+  input {
+    font-size: 14px;
+    letter-spacing: 1px;
+    padding: 13px 10px;
+    width: -webkit-fill-available;
+    border-radius: 8px;
+    border: 1px solid #e4e4e7;
+  }
+  input:focus {
+    outline: none;
+    border: 1px solid #a1a1aa;
+  }
+`;
+
+export const StyledForm = styled(Box)`
+  margin: 20px 0 30px 0;
+`;
+
+export const StyledButton = styled(Button)``;
