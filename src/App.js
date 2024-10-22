ba16e0be9d043114e3bbe728f5dfa8e410eaa197
subashprj55 <Sonikgau@1gau>
2024-10-22 22:14:06 +0545

    profil page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import BugAboutUs from "./Pages/BugAboutUs";
-import BugTest from "./Pages/BugTest";
 import "./App.css";
 import BugHome from "Pages/BugHome";
 import Bug404 from "Pages/Bug404";
 import BugLogin from "Pages/BugLogin";
 import BugSignup from "Pages/BugSignup";
+import BugProfile from "Pages/BugProfile";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<BugHome />} />
+        <Route path="profile" element={<BugProfile />} />
         <Route path="transaction" element={<BugAboutUs />} />
-        <Route path="test" element={<BugTest />} />
         <Route path="login" element={<BugLogin />} />
         <Route path="signup" element={<BugSignup />} />
         <Route path="*" element={<Bug404 />} />
---
+++ src/Pages/BugProfile/index.js
@@ -0,0 +1,131 @@
+import BugNavContainer from "Components/BugNavContainer";
+import React from "react";
+import {
+  StyledAvatar,
+  StyledBadgesBox,
+  StyledBadgesChip,
+  StyledBadgesSection,
+  StyledButton,
+  StyledButtonBox,
+  StyledChip,
+  StyledDetails,
+  StyledDiv,
+  StyledHeaderBox,
+  StyledProfileBox,
+  StyledProfileHead,
+  StyledProfilePage,
+  StyledRecentActivitySection,
+  StyledStack,
+  StyledTypography,
+} from "./style";
+import profileAvatar from "Images/profileAvatar.png";
+import BugTable from "Components/BugTable";
+import CupIcon from "Images/cup.png";
+import Shield from "Images/shield.png";
+import Mark from "Images/mark.png";
+
+const BugProfile = () => {
+  return (
+    <>
+      <BugNavContainer>
+        <StyledProfilePage>
+          <TitleSection />
+          <BadgesSection />
+        </StyledProfilePage>
+      </BugNavContainer>
+    </>
+  );
+};
+
+export default BugProfile;
+
+const TitleSection = () => {
+  return (
+    <>
+      <StyledHeaderBox>
+        <StyledTypography variant="h1">Hunter Profile</StyledTypography>
+      </StyledHeaderBox>
+
+      <StyledStack>
+        <StyledProfileBox>
+          <StyledProfileHead>
+            <StyledDiv>
+              <StyledAvatar src={profileAvatar} />
+              <StyledTypography>Alice Wonderhack</StyledTypography>
+            </StyledDiv>
+          </StyledProfileHead>
+
+          <StyledProfileHead>
+            <StyledChip label="Rank #1" />
+          </StyledProfileHead>
+
+          <StyledDetails className="space">
+            <StyledDiv>
+              <StyledTypography variant="h2" className="font">
+                5000
+              </StyledTypography>
+              <StyledTypography variant="h6">XLOP Earned</StyledTypography>
+            </StyledDiv>
+
+            <StyledDiv>
+              <StyledTypography variant="h2" className="font">
+                42
+              </StyledTypography>
+              <StyledTypography>Bug Reported</StyledTypography>
+            </StyledDiv>
+          </StyledDetails>
+
+          <StyledDetails>
+            <StyledDiv>
+              <StyledTypography variant="h2" className="font">
+                95%
+              </StyledTypography>
+              <StyledTypography>Success Rate</StyledTypography>
+            </StyledDiv>
+
+            <StyledDiv>
+              <StyledTypography variant="h2" className="font">
+                3
+              </StyledTypography>
+              <StyledTypography>Badges Earned</StyledTypography>
+            </StyledDiv>
+          </StyledDetails>
+        </StyledProfileBox>
+
+        <StyledRecentActivitySection>
+          <StyledTypography variant="h2">Recent Activity</StyledTypography>
+          <StyledTypography variant="h6">
+            Latest bug reports and rewards
+          </StyledTypography>
+          <BugTable />
+        </StyledRecentActivitySection>
+      </StyledStack>
+    </>
+  );
+};
+
+const BadgesSection = () => {
+  return (
+    <>
+      <StyledBadgesSection>
+        <StyledTypography variant="h2">Badges</StyledTypography>
+        <StyledTypography variant="h6">
+          Achievements and recognitions
+        </StyledTypography>
+        <StyledBadgesBox>
+          <StyledBadgesChip icon={<img src={CupIcon} />} label="Top Hunter" />
+          <StyledBadgesChip
+            className="badge"
+            icon={<img src={Mark} />}
+            label="Critical Bug Finding"
+          />
+          <StyledBadgesChip icon={<img src={Shield} />} label="Speed Demon" />
+        </StyledBadgesBox>
+      </StyledBadgesSection>
+
+      <StyledButtonBox>
+        <StyledButton variant="contained">Back to Leader Board</StyledButton>
+      </StyledButtonBox>
+    </>
+  );
+};
