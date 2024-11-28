30ff1438b09e36047ad303d2d50876c6b2a1d882
subashprj55 <Sonikgau@1gau>
2024-11-28 19:58:37 +0545

    fomvalidation using react hook form

--- src/Pages/BugSignup/index.js
+++ src/Pages/BugSignup/index.js
@@ -1,14 +1,21 @@
+import { Controller, useForm } from "react-hook-form";
 import BugBox from "Components/BugBox";
-import BugInputField from "Components/BugInputField";
-import BugSelectField from "Components/BugSelectFiled";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Industries, primarySkills } from "./data";
 import {
   StyledBottomBox,
   StyledBox,
   StyledCheckBox,
+  StyledErrorMessage,
+  StyledFooterDiv,
+  StyledForm,
+  StyledFormControl,
   StyledInputBox,
+  StyledInputField,
+  StyledInputLabel,
+  StyledMenuItem,
+  StyledSelect,
   StyledSignupBox,
   StyledSignupButton,
   StyledSignupStack,
@@ -18,27 +25,34 @@ import {
 } from "./style";
 
 const BugSignup = () => {
-  const [alignment, setAlignment] = useState("Hunter");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    reset,
+  } = useForm();
 
+  const [checked, setChecked] = React.useState(false);
+  const [error, setError] = useState(false);
+  const [alignment, setAlignment] = useState("Hunter");
   const navigate = useNavigate();
 
   const handleAlignment = (event, newAlignment) => {
     setAlignment(newAlignment);
+    setChecked(false);
+    setError(false);
+    reset();
   };
 
-  //for hunter
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [hunterSkill, setHunterSkill] = React.useState("");
-
-  //for client
-  const [companyName, setCompanyName] = useState("");
-  const [businessEmail, setBusinessEmail] = useState("");
-  const [clientPassword, setClientPassword] = useState("");
-  const [industryName, setIndustryName] = useState("");
-
-  const [checked, setChecked] = React.useState(false);
+  const onSubmit = (data) => {
+    if (!checked) {
+      setError(true);
+    } else {
+      setError(false);
+      console.log("Form Data:", data);
+    }
+  };
 
   const renderInputFields = (alignmentValue) => {
     if (alignmentValue === "Hunter") {
@@ -48,47 +62,108 @@ const BugSignup = () => {
             <StyledSignupTypography variant="h3" className="space">
               Full Name
             </StyledSignupTypography>
-            <BugInputField
-              placeholder="Enter your Username"
-              value={name}
-              setValue={setName}
+            <StyledInputField
+              id="name"
+              name="hunterName"
+              placeholder="Enter your Full Name"
+              {...register("hunterName", {
+                required: "Hunter name is required",
+                minLength: {
+                  value: 3,
+                  message: "Name must be at least 3 characters",
+                },
+              })}
             />
+            {errors.hunterName && (
+              <StyledErrorMessage>
+                {errors.hunterName.message}
+              </StyledErrorMessage>
+            )}
           </StyledBox>
 
           <StyledBox>
             <StyledSignupTypography variant="h3" className="space">
               Email
             </StyledSignupTypography>
-            <BugInputField
+            <StyledInputField
+              id="email"
+              name="email"
               type="email"
-              placeholder="Enter your Username"
-              value={email}
-              setValue={setEmail}
+              placeholder="Enter your email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Invalid email format",
+                },
+              })}
             />
+            {errors.email && (
+              <StyledErrorMessage>{errors.email.message}</StyledErrorMessage>
+            )}
           </StyledBox>
 
           <StyledBox>
             <StyledSignupTypography variant="h3" className="space">
               Password
             </StyledSignupTypography>
-            <BugInputField
+            <StyledInputField
+              id="password"
               type="password"
-              placeholder="Enter your password"
-              value={password}
-              setValue={setPassword}
+              name="password"
+              placeholder="Enter your Password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters",
+                },
+              })}
             />
+            {errors.password && (
+              <StyledErrorMessage>{errors.password.message}</StyledErrorMessage>
+            )}
           </StyledBox>
 
           <StyledBox>
             <StyledSignupTypography variant="h3" className="space">
               Primary Skills
             </StyledSignupTypography>
-            <BugSelectField
-              label={"Select your primary skill"}
-              options={primarySkills}
-              selectValue={hunterSkill}
-              setSelectValue={setHunterSkill}
-            />
+
+            <StyledFormControl fullWidth>
+              <StyledInputLabel id="demo-simple-select-label">
+                Select your primary skill
+              </StyledInputLabel>
+              <Controller
+                name="primarySkill"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: "Please select one option", // Validation rule for required field
+                }}
+                render={({ field, fieldState: { error } }) => (
+                  <>
+                    <StyledSelect
+                      {...field}
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      label="Select your primary skill"
+                    >
+                      {primarySkills.map(({ id, name, value }) => (
+                        <StyledMenuItem key={id} value={value}>
+                          {name}
+                        </StyledMenuItem>
+                      ))}
+                    </StyledSelect>
+                  </>
+                )}
+              />
+            </StyledFormControl>
+            {errors.primarySkill && (
+              <StyledErrorMessage>
+                {errors.primarySkill.message}
+              </StyledErrorMessage> // Display error message
+            )}
           </StyledBox>
         </>
       );
@@ -99,47 +174,109 @@ const BugSignup = () => {
             <StyledSignupTypography variant="h3" className="space">
               Company Name
             </StyledSignupTypography>
-            <BugInputField
-              placeholder="Enter your Username"
-              value={companyName}
-              setValue={setCompanyName}
+            <StyledInputField
+              id="name"
+              name="clientName"
+              placeholder="Enter your Company Name"
+              {...register("clientName", {
+                required: "Company Name is required",
+                minLength: {
+                  value: 3,
+                  message: "Company Name must be at least 3 characters",
+                },
+              })}
             />
+            {errors.clientName && (
+              <StyledErrorMessage>
+                {errors.clientName.message}
+              </StyledErrorMessage>
+            )}
           </StyledBox>
 
           <StyledBox>
             <StyledSignupTypography variant="h3" className="space">
               Business Email
             </StyledSignupTypography>
-            <BugInputField
+            <StyledInputField
+              id="userEmail"
+              name="userEmail"
               type="email"
-              placeholder="Enter your Username"
-              value={businessEmail}
-              setValue={setBusinessEmail}
+              placeholder="Enter your email"
+              {...register("userEmail", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Invalid email format",
+                },
+              })}
             />
+            {errors.userEmail && (
+              <StyledErrorMessage>
+                {errors.userEmail.message}
+              </StyledErrorMessage>
+            )}
           </StyledBox>
 
           <StyledBox>
             <StyledSignupTypography variant="h3" className="space">
               Password
             </StyledSignupTypography>
-            <BugInputField
+            <StyledInputField
+              id="password"
               type="password"
-              placeholder="Enter your password"
-              value={clientPassword}
-              setValue={setClientPassword}
+              name="userPassword"
+              placeholder="Enter your Password"
+              {...register("userPassword", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters",
+                },
+              })}
             />
+            {errors.userPassword && (
+              <StyledErrorMessage>
+                {errors.userPassword.message}
+              </StyledErrorMessage>
+            )}
           </StyledBox>
 
           <StyledBox>
             <StyledSignupTypography variant="h3" className="space">
-              Industry.
+              Industry
             </StyledSignupTypography>
-            <BugSelectField
-              label={"Select your industry"}
-              options={Industries}
-              selectValue={industryName}
-              setSelectValue={setIndustryName}
-            />
+            <StyledFormControl fullWidth>
+              <StyledInputLabel id="demo-simple-select-label">
+                Select your industry
+              </StyledInputLabel>
+              <Controller
+                name="industry"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: "Please select one option", // Validation rule for required field
+                }}
+                render={({ field, fieldState: { error } }) => (
+                  <>
+                    <StyledSelect
+                      {...field}
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      label="Select your industry"
+                    >
+                      {Industries.map(({ id, name, value }) => (
+                        <StyledMenuItem key={id} value={value}>
+                          {name}
+                        </StyledMenuItem>
+                      ))}
+                    </StyledSelect>
+                  </>
+                )}
+              />
+            </StyledFormControl>
+            {errors.industry && (
+              <StyledErrorMessage>{errors.industry.message}</StyledErrorMessage> // Display error message
+            )}
           </StyledBox>
         </>
       );
@@ -151,48 +288,55 @@ const BugSignup = () => {
       <StyledSignupStack>
         <BugBox>
           <StyledSignupBox>
-            <StyledSignupTypography variant="h1" className="center">
-              Sign Up for Xlooop Bug Bounty
-            </StyledSignupTypography>
-            <StyledSignupTypography variant="h6" className="center">
-              Join our community as a hunter or client
-            </StyledSignupTypography>
-
-            <StyledBottomBox>
-              <StyledToggleButtonGroup
-                value={alignment}
-                exclusive
-                onChange={handleAlignment}
-                aria-label="text alignment"
-              >
-                <StyledToggleButton value="Hunter" aria-label="left aligned">
-                  <StyledSignupTypography>Hunter</StyledSignupTypography>
-                </StyledToggleButton>
-                <StyledToggleButton value="Client" aria-label="right aligned">
-                  <StyledSignupTypography>Client</StyledSignupTypography>
-                </StyledToggleButton>
-              </StyledToggleButtonGroup>
-            </StyledBottomBox>
-
-            <StyledInputBox>{renderInputFields(alignment)}</StyledInputBox>
-
-            <StyledBottomBox>
-              <StyledCheckBox
-                color="default"
-                checked={checked}
-                onChange={(e) => setChecked(e.target.checked)}
-              />
-              <StyledSignupTypography>
-                I agree to the terms and conditions
+            <StyledForm onSubmit={handleSubmit(onSubmit)}>
+              <StyledSignupTypography variant="h1" className="center">
+                Sign Up for Xlooop Bug Bounty
               </StyledSignupTypography>
-            </StyledBottomBox>
+              <StyledSignupTypography variant="h6" className="center">
+                Join our community as a hunter or client
+              </StyledSignupTypography>
 
-            <StyledSignupButton
-              variant="contained"
-              onClick={() => navigate("/profile")}
-            >
-              Sign Up as {alignment}
-            </StyledSignupButton>
+              <StyledBottomBox>
+                <StyledToggleButtonGroup
+                  value={alignment}
+                  exclusive
+                  onChange={handleAlignment}
+                  aria-label="text alignment"
+                >
+                  <StyledToggleButton value="Hunter" aria-label="left aligned">
+                    <StyledSignupTypography>Hunter</StyledSignupTypography>
+                  </StyledToggleButton>
+                  <StyledToggleButton value="Client" aria-label="right aligned">
+                    <StyledSignupTypography>Client</StyledSignupTypography>
+                  </StyledToggleButton>
+                </StyledToggleButtonGroup>
+              </StyledBottomBox>
+
+              <StyledInputBox>{renderInputFields(alignment)}</StyledInputBox>
+
+              <StyledBottomBox>
+                <StyledCheckBox
+                  className={error && !checked ? "error" : ""}
+                  color="default"
+                  checked={checked}
+                  onChange={(e) => setChecked(e.target.checked)}
+                />
+                <StyledSignupTypography>
+                  I agree to the terms and conditions
+                </StyledSignupTypography>
+              </StyledBottomBox>
+
+              <StyledSignupButton type="submit" variant="contained">
+                Sign Up as {alignment}
+              </StyledSignupButton>
+            </StyledForm>
+
+            <StyledFooterDiv>
+              <StyledSignupTypography>
+                Already have an account{" "}
+              </StyledSignupTypography>
+              <Link to="/login">?Login</Link>
+            </StyledFooterDiv>
           </StyledSignupBox>
         </BugBox>
       </StyledSignupStack>
--- src/Pages/BugSignup/style.js
+++ src/Pages/BugSignup/style.js
@@ -2,6 +2,10 @@ import {
   Box,
   Button,
   Checkbox,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
   ToggleButton,
   ToggleButtonGroup,
   Typography,
@@ -17,7 +21,7 @@ export const StyledSignupStack = styled(Stack)`
 
 export const StyledSignupBox = styled(Box)`
   width: fit-content;
-  padding: 30px 50px;
+  padding: 30px 50px 0px 50px;
 `;
 
 export const StyledSignupTypography = styled(Typography)`
@@ -35,12 +39,16 @@ export const StyledInputBox = styled(Box)`
 `;
 
 export const StyledBox = styled(Box)`
-  margin-top: 15px;
+  margin-top: 20px;
 `;
 
 export const StyledSignupButton = styled(Button)``;
 
-export const StyledCheckBox = styled(Checkbox)``;
+export const StyledCheckBox = styled(Checkbox)`
+  &.error {
+    color: red !important;
+  }
+`;
 
 export const StyledBottomBox = styled(Box)`
   margin-top: 10px;
@@ -56,3 +64,57 @@ export const StyledToggleButton = styled(ToggleButton)`
     font-size: 13px !important;
   }
 `;
+
+export const StyledFooterDiv = styled(Box)`
+  margin: 20px 0;
+  display: flex;
+  justify-content: center;
+`;
+
+export const StyledForm = styled.form`
+  padding: 5px 0;
+`;
+
+export const StyledInputField = styled.input`
+  font-size: 14px;
+  letter-spacing: 1px;
+  padding: 13px 10px;
+  width: 93%;
+  border-radius: 8px;
+  border: 1px solid #e4e4e7;
+
+  &:focus {
+    outline: none;
+    border: 1px solid #a1a1aa;
+  }
+`;
+
+export const StyledErrorMessage = styled.p`
+  position: absolute;
+  color: red;
+`;
+
+export const StyledFormControl = styled(FormControl)`
+  padding-top: 5px !important;
+  padding: 13px 10px;
+  .css-113d811-MuiFormLabel-root-MuiInputLabel-root.Mui-focused {
+    margin-top: 6px;
+    color: gray !important;
+  }
+  .css-sc8y68-MuiInputBase-root-MuiOutlinedInput-root-MuiSelect-root.Mui-focused
+    .MuiOutlinedInput-notchedOutline {
+    border: 1px solid #a1a1aa;
+  }
+  .css-113d811-MuiFormLabel-root-MuiInputLabel-root {
+    margin-top: 6px;
+  }
+  .css-w76bbz-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input {
+    padding: 12px 10px;
+  }
+`;
+
+export const StyledSelect = styled(Select)``;
+
+export const StyledMenuItem = styled(MenuItem)``;
+
+export const StyledInputLabel = styled(InputLabel)``;
